cf46d7ad3e92ed1debdfce8f602f22a68be27c1a
andycapn <aundreycap@gmail.com>
2021-03-01 16:28:27 +0200

    Feat: Updated home page layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "../custom.scss"
-import {css} from "@emotion/react"
+import { css } from "@emotion/react"
 import styled from "@emotion/styled"
 import { Power3, TimelineLite } from "gsap"
 import BackgroundImage from "gatsby-background-image"
@@ -13,6 +13,8 @@ import {
   Button,
   MainDiv,
 } from "../components/MyStyledComonents"
+import Img from "gatsby-image"
+import Image from "../components/image"
 
 const ImageBackground = styled(BackgroundImage)`
   background-position: center;
@@ -47,39 +49,20 @@ const IndexPage = () => {
   // GSAP Animations
 
   useEffect(() => {
-    //Heading Animation
-    tl.from(
-      heading1,
-      1,
-      {
-        y: 10,
-        opacity: 0,
-        ease: Power3.easeOut,
-      },
-      1.4
-    )
-      .from(body1, 1, { y: 10, opacity: 0, ease: Power3.easeOut }, 2.4)
-      .paused()
+    // //Heading Animation
+    // tl.from(
+    //   heading1,
+    //   1,
+    //   {
+    //     y: 10,
+    //     opacity: 0,
+    //     ease: Power3.easeOut,
+    //   },
+    //   1.4
+    // )
+    //   .from(body1, 1, { y: 10, opacity: 0, ease: Power3.easeOut }, 2.4)
+    //   .paused()
   }, [tl])
-
-  const { image } = useStaticQuery(graphql`
-    query {
-      image: file(relativePath: { eq: "home-bg.jpg" }) {
-        sharp: childImageSharp {
-          fluid(quality: 90) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
-        }
-      }
-    }
-  `)
-
-  // Overlay Background Image with Gradient
-  const BackgroundImages = [
-    image.sharp.fluid,
-    `linear-gradient(90deg, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.7) 35%, rgba(0,0,0,0.7) 100%)`,
-    `linear-gradient(176deg, rgba(108,141,250,1) 0%, rgba(104,182,239,0.29) 58%, rgba(97,244,222,1) 100%)`,
-  ].reverse()
 
   return (
     <Layout>
@@ -87,72 +70,64 @@ const IndexPage = () => {
         title="Home"
         description="Experienced Web and Software Developer based in Lusaka Zambia"
       />
-      <ImageBackground
-        tag={`section`}
-        fluid={BackgroundImages}
+      <MainDiv
         css={css`
-          background-color: #0b132b;
-          opacity: 0;
+          display: grid;
           min-height: 100vh;
+          .banner-image {
+            min-width: 325px;
+            margin-bottom: 40px;
+          }
+          .header {
+            font-size: 74px;
+          }
+          .btn-container {
+            display: flex;
+            gap: 20px;
+            justify-content: center;
+            margin: 20px 0;
+          }
+          @media screen and (min-width: 768px) {
+            grid-template-columns: repeat(auto-fill, minmax(300px, 49%));
+            place-items: center;
+            grid-column-gap: 10px;
+            .banner-image {
+              min-width: 520px;
+            }
+            .btn-container {
+              justify-content: unset;
+            }
+          }
         `}
       >
-        <MainDiv style={{ height: "100vh" }}>
-          <div
-            css={css`
-              position: relative;
-              top: 50px;
-              display: flex;
-              flex-direction: column;
-              align-items: center;
-              @media screen and (min-width: 768px) {
-                top: 150px;
-              }
-              @media screen and (min-width: 1440px) {
-                align-items: unset;
-              }
-            `}
-          >
-            <Header
-              css={css`
-                @media screen and (max-width: 2560px) {
-                  font-size: 100px;
-                }
-                @media screen and (max-width: 1024px) {
-                  font-size: 80px;
-                }
-              `}
-              ref={el => (heading1 = el)}
-            >
-              Hi, My name is Andrew.
-            </Header>
-            <BodyText
-              css={css`
-                max-width: 700px;
-                @media screen and (max-width: 1366px) {
-                  text-align: center;
-                }
-              `}
-              ref={el => (body1 = el)}
-              className="typing"
-            >
-              I build interesting and innovative online experiences. Click below
-              to learn more or see some of my work.
-            </BodyText>
-            <span>
+        <div className="column">
+          <Img
+            fluid={Image().bannerImage.childImageSharp.fluid}
+            className="banner-image"
+          />
+        </div>
+        <div className="column">
+          <Header className={"header"}>Hey, My Name is Andrew.</Header>
+          <BodyText>
+            I build interesting and innovative online experiences. Click below
+            to learn more or see some of my work.
+            <div className="btn-container">
+              <Button href="#" style={{ backgroundColor: "black" }}>
+                View My Portfolio
+              </Button>
               <Button
-                css={ButtonStyling.buttonOne}
-                to={`/portfolio`}
-                style={{ marginLeft: "0" }}
+                href="#"
+                style={{
+                  backgroundColor: "black",
+                  color: "white",
+                }}
               >
-                View Portfolio
+                Learn More
               </Button>
-              <Button css={ButtonStyling.buttonTwo} to={`/about`}>
-                About Me
-              </Button>
-            </span>
-          </div>
-        </MainDiv>
-      </ImageBackground>
+            </div>
+          </BodyText>
+        </div>
+      </MainDiv>
     </Layout>
   )
 }
