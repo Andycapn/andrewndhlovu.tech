34020cdcc3e98fb0b531bc7d4275a6a3dbf9467d
andycapn <aundreycap@gmail.com>
2021-03-01 16:32:16 +0200

    Chore

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/icons/earth.png`, // This path is relative to the root of the site.
+        icon: `src/images/icons/earth.svg`, // This path is relative to the root of the site.
       },
     },
     {
